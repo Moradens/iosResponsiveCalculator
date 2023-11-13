53b1e714b4daa3c7a988329300584d877d6e719c
Moradens <141424710+Moradens@users.noreply.github.com>
2023-11-13 15:48:03 +0100

    Initial Commit

---
+++ Kalkulacka/KalkulackaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalkulackaApp.swift
+//  Kalkulacka
+//
+//  Created by Denis Moravec on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct KalkulackaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
